ba515557f92b712fa170dd6a0257f5cf6d00dc25
Mathew Manoj (root) <64970593+mastermach50@users.noreply.github.com>
2026-03-20 11:33:08 +0000

    Correct logic in main function, update jetstream.proto, add connection notificatio

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,55 +1,26 @@
-use tauri::{
-    menu::{Menu, MenuItem},
-    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent}, Manager,
-};
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-    tauri::Builder::default()
-        .setup(|app| {
-            let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-            let menu = Menu::with_items(app, &[&quit])?;
+mod mdns;
+mod websocket;
+mod certs;
 
-            let tray = TrayIconBuilder::new()
-                .icon(app.default_window_icon().unwrap().clone())
-                .tooltip("Disconnected")
-                .menu(&menu)
-                .on_menu_event(|app, event| match event.id.as_ref() {
-                    "quit" => {
-                        app.exit(0);
-                    }
-                    _ => {}
-                })
-                .on_tray_icon_event(|tray, event| {
-                    if let TrayIconEvent::Click {
-                        button: MouseButton::Left,
-                        button_state: MouseButtonState::Up,
-                        ..
-                    } = event
-                    {
-                        let app = tray.app_handle();
-                        if let Some(window) = app.get_webview_window("main") {
-                            let _ = window.show();
-                            let _ = window.set_focus();
-                        }
-                    }
-                })
-                .build(app)?;
+// const VERSION: &str = env!("CARGO_PKG_VERSION");
+pub const PORT: u16 = 8000;
 
-            app.manage(tray);
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![update_tray_tooltip])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
+#[tokio::main]
+async fn main() {
+    // Initialize logging
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("jetstream_desktop=debug")).init();
 
-#[tauri::command]
-fn update_tray_tooltip(
-    app: tauri::AppHandle,
-    connected: bool,
-) {
-    if let Some(tray) = app.tray_by_id("main") {
-        let msg = if connected { "Connected" } else { "Disconnected" };
-        let _ = tray.set_tooltip(Some(msg));
-    }
+    // Start the mdns service
+    let _service = mdns::start_mdns_responder("Mathew's JetStream".to_string(), PORT);
+
+    // Start the jetstream server
+    websocket::start_server(PORT).await;
+
+    // Run the Tauri application
+    jetstream_desktop_lib::run();
+
+    loop {}
 }
