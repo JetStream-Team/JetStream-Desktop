201f6091ed3f0f8c89ef87bb77eae8345dd4a9d8
Mathew Manoj (root) <64970593+mastermach50@users.noreply.github.com>
2026-03-26 19:20:18 +0000

    Feat: Add clipboard sending, global outbox, window hiding

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,11 @@
 use tauri::{
-    menu::{Menu, MenuItem},
-    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent}, Manager,
+    AppHandle, Manager, menu::{Menu, MenuItem}, tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent}
 };
 
-pub mod mdns;
-pub mod websocket;
 pub mod certs;
 pub mod protobuf_message;
 pub mod message_handlers;
+pub mod websocket;
 
 pub const SERVER_NAME: &str = "Mathew's JetStream";
 pub const PORT: u16 = 8000;
@@ -16,40 +14,32 @@ pub const PORT: u16 = 8000;
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
-            let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-            let menu = Menu::with_items(app, &[&quit])?;
-
-            let tray = TrayIconBuilder::with_id("main")
-                .icon(app.default_window_icon().unwrap().clone())
-                .tooltip("Disconnected")
-                .menu(&menu)
-                .on_menu_event(|app, event| match event.id.as_ref() {
-                    "quit" => {
-                        app.exit(0);
-                    }
-                    _ => {}
-                })
-                .on_tray_icon_event(|tray, event| {
-                    if let TrayIconEvent::Click {
-                        button: MouseButton::Left,
-                        button_state: MouseButtonState::Up,
-                        ..
-                    } = event
-                    {
-                        let app = tray.app_handle();
-                        if let Some(window) = app.get_webview_window("main") {
-                            let _ = window.show();
-                            let _ = window.set_focus();
-                        }
-                    }
-                })
-                .build(app)?;
-
-            app.manage(tray);
-
             let app_handle = app.handle().clone();
-            tauri::async_runtime::spawn(async move {
-                websocket::start_server(PORT, app_handle).await;
+            let args: Vec<String> = std::env::args().collect();
+            let window = app_handle.get_webview_window("main")
+                    .expect("Failed to get webview window");
+            let window_clone = window.clone();
+
+            // Start minimized if argument is passed
+            if args.contains(&"--start-minimized".to_string()) {
+                window.hide()
+                    .expect("Failed to hide webview window");
+            }
+
+            // Minimize to tray on window close
+            window.on_window_event(move |event| {
+                if let tauri::WindowEvent::CloseRequested { api, .. } = event {
+                    api.prevent_close();
+                    window_clone.hide()
+                        .expect("Failed to hide webview window");
+                }
+            });
+
+            build_tray(&app_handle)?;
+
+            // Start the server in a seperate thread
+            tokio::spawn(async move {
+                websocket::start_server(app_handle).await;
             });
 
             Ok(())
@@ -59,3 +49,76 @@ pub fn run() {
         .expect("error while running tauri application");
 }
 
+fn build_tray(app: &tauri::AppHandle) -> Result<(), tauri::Error> {
+    let show_hide = MenuItem::with_id(
+        app,
+        "show_hide",
+        "Show/Hide App",
+        true,
+        None::<&str>
+    )?;
+    let send_clipboard = MenuItem::with_id(
+        app,
+        "send_clipboard",
+        "Send Clipboard",
+        true,
+        None::<&str>
+    )?;
+    let quit = MenuItem::with_id(
+        app,
+        "quit",
+        "Quit",
+        true,
+        None::<&str>
+    )?;
+
+    let menu = Menu::with_items(
+        app,
+        &[&show_hide, &send_clipboard, &quit]
+    )?;
+
+    TrayIconBuilder::new()
+        .icon(app.default_window_icon().unwrap().clone())
+        .tooltip("Disconnected")
+        .title("Disconnected")
+        .menu(&menu)
+        .show_menu_on_left_click(false) // We handle left-click manually
+        .on_menu_event(|app, event| match event.id.as_ref() {
+            "show_hide" => {
+                toggle_window_visibility(app);
+            },
+            "send_clipboard" => {
+                message_handlers::clipboard::send_clipboard();
+            }
+            "quit" => {
+                app.exit(0);
+            }
+            _ => {}
+        })
+        .on_tray_icon_event(|tray, event| {
+            if let TrayIconEvent::Click {
+                button: MouseButton::Left,
+                button_state: MouseButtonState::Up,
+                ..
+            } = event
+            {
+                let app = tray.app_handle();
+                toggle_window_visibility(app);
+            }
+        })
+        .build(app)?;
+
+
+    Ok(())
+}
+
+fn toggle_window_visibility(app: &AppHandle) {
+    if let Some(window) = app.get_webview_window("main") {
+        if window.is_visible().unwrap_or(false) {
+            let _ = window.hide();
+        } else {
+            let _ = window.show();
+            let _ = window.set_focus();
+        }
+    }
+}
--- src-tauri/src/message_handlers/clipboard.rs
+++ src-tauri/src/message_handlers/clipboard.rs
@@ -1,16 +1,34 @@
 use log::debug;
-use lazy_static::lazy_static;
+use std::sync::LazyLock;
 use arboard::Clipboard;
+use prost::Message;
 use tokio::sync::Mutex;
 
-use crate::protobuf_message::pb;
+use crate::{protobuf_message::pb, websocket::OUTBOX};
 
-lazy_static! {
-    static ref clipboard: Mutex<Clipboard> = Mutex::new(Clipboard::new().unwrap());
-}
+static CLIPBOARD: LazyLock<Mutex<Clipboard>> = LazyLock::new(||Mutex::new(Clipboard::new().unwrap()));
 
 pub async fn handle_clipboard(cb: pb::Clipboard) {
     debug!("--- Clipboard Sync ---");
     debug!("Content: {}", cb.content);
-    clipboard.lock().await.set_text(&cb.content).unwrap();
+    CLIPBOARD.lock().await.set_text(&cb.content).unwrap();
+}
+
+pub fn send_clipboard() {
+    tokio::spawn(async {
+
+        if !OUTBOX.is_connected().await { return; }
+
+        let text = CLIPBOARD.lock().await
+          .get_text().expect("Failed to get text from clipboard");
+        let wrapper = pb::MessageWrapper {
+            message: Some(pb::message_wrapper::Message::Clipboard(
+                pb::Clipboard { content: text }
+            ))
+        };
+
+        let encoded = wrapper.encode_to_vec();
+        OUTBOX.send(encoded.into()).await.expect("Failed to send clipboard");
+        debug!("Set clipboard");
+    });
 }
