012f25fb54beaabba052e06f1cd2c3674eca0ad3
Mathew Manoj (root) <64970593+mastermach50@users.noreply.github.com>
2026-03-27 04:38:46 +0000

    More accurate logging timestamps

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,26 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 pub mod mdns;
+use chrono::Local;
 use jetstream_desktop_lib::{SERVER_NAME, PORT};
+use std::io::Write;
 
 #[tokio::main]
 async fn main() {
     // Setup env_logger
     env_logger::Builder::from_env(
         env_logger::Env::default().default_filter_or("jetstream_desktop=debug")
-    ).init();
+    )
+    .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} [{}] {}",
+                Local::now().format("%Y-%m-%d %H:%M:%S%.3f %z"),
+                record.level(),
+                record.args()
+            )
+        })
+    .init();
 
     // Start mDNS responder in seperate thread
     let _service = mdns::start_mdns_responder(
